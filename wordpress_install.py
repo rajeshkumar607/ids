6df7b51e6f45b119be5dce0e5f3ed563bb05bf60
rajeshsaggu607 <rajeshsaggu607@gmail.com>
2024-01-15 15:45:55 +0530

    added

--- wordpress_install.py
+++ wordpress_install.py
@@ -55,9 +55,10 @@ def ngninx_config(input_file, output_file, old_word, new_word):
         command = f'cp /etc/php/8.0/fpm/pool.d/www.conf /etc/php/8.0/fpm/pool.d/{site_name}.conf'
         subprocess.run(command, shell=True)
         command = f"sed -e '4s/www/{site_name}/' -i -e '23s/www-data/{site_name}/' -e '36s/php8.0-fpm/php8.0-fpm_{site_name}/' /etc/php/8.0/fpm/pool.d/{site_name}.conf "
+        subprocess.run(command, shell=True)
         # testing php-fpm and restart 
         subprocess.run(["service", "php8.0-fpm", "restart"]) 
-        subprocess.run(command, shell=True)
+        
     except FileNotFoundError:
         print(f"Error: File '{input_file}' not found.")
     except Exception as e:
